d4ffbd6cc78aef6092322ae738f7f95559b8ca77
dondaha <1656982710@qq.com>
2024-12-17 00:28:36 +0800

    epoch8达到83.3%

--- model.py
+++ model.py
@@ -5,9 +5,12 @@ import torch.nn.functional as F
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        conv1_size = 5
+        conv2_size = 5
+        conv3_size = 3
+        self.conv1 = nn.Conv2d(3, 16, conv1_size, padding=conv1_size//2)
+        self.conv2 = nn.Conv2d(16, 32, conv2_size, padding=conv2_size//2)
+        self.conv3 = nn.Conv2d(32, 64, conv3_size, padding=conv3_size//2)
         self.pool = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(64 * 37 * 37, 512)
         self.fc2 = nn.Linear(512, 3)
@@ -19,4 +22,53 @@ class SimpleCNN(nn.Module):
         x = x.view(-1, 64 * 37 * 37)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
+        return x
+    
+    
+class TinyVGG(nn.Module):
+    """
+    Model architecture copying TinyVGG from:
+    https://poloclub.github.io/cnn-explainer/
+    """
+
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3, 
+                      stride=1, 
+                      padding=1), 
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2) 
+        )
+
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*16*16,
+                      out_features=output_shape)
+        )
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv_block_1(x)
+        # print(x.shape)
+        x = self.conv_block_2(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        # print(x.shape)
         return x
--- train.py
+++ train.py
@@ -6,10 +6,35 @@ from torchvision import datasets, transforms
 from model import SimpleCNN
 from tqdm import tqdm
 
+import torch
+import numpy as np
+import random
+import os
+ 
+def setup_seed(seed=3407):
+    random.seed(seed)  # Python的随机性
+    os.environ['PYTHONHASHSEED'] = str(seed)  # 设置Python哈希种子，为了禁止hash随机化，使得实验可复现
+    np.random.seed(seed)  # numpy的随机性
+    torch.manual_seed(seed)  # torch的CPU随机性，为CPU设置随机种子
+    torch.cuda.manual_seed(seed)  # torch的GPU随机性，为当前GPU设置随机种子
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.   torch的GPU随机性，为所有GPU设置随机种子
+    torch.backends.cudnn.deterministic = True # 选择确定性算法
+    torch.backends.cudnn.benchmark = False # if benchmark=True, deterministic will be False
+    torch.backends.cudnn.enabled = False
+    
+
+setup_seed(1231312)
+
+# Check if CUDA is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f'Using device: {device}')
+
 # Data transformations
 transform = transforms.Compose([
     transforms.Resize((300, 300)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    # 正则化
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 # Load datasets
@@ -24,13 +49,13 @@ print(f'Training samples: {len(train_dataset)}, Testing samples: {len(test_datas
 print(f'Classes: {train_dataset.classes}')
 
 # Initialize model, loss function, and optimizer
-model = SimpleCNN()
+model = SimpleCNN().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # Training loop
 num_epochs = 10
-test_interval = 1  # Test the model every 2 epochs
+test_interval = 1  # Test the model every epoch
 
 for epoch in range(num_epochs):
     model.train()
@@ -38,6 +63,7 @@ for epoch in range(num_epochs):
     progress_bar = tqdm(train_loader, desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch')
     
     for inputs, labels in progress_bar:
+        inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
         outputs = model(inputs)
         loss = criterion(outputs, labels)
@@ -55,6 +81,7 @@ for epoch in range(num_epochs):
         total = 0
         with torch.no_grad():
             for inputs, labels in test_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
                 outputs = model(inputs)
                 _, predicted = torch.max(outputs, 1)
                 total += labels.size(0)
