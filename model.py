2618db92a39c1d1ad2007693650cb68788ed504e
dondaha <1656982710@qq.com>
2024-12-17 00:53:21 +0800

    epoch5达到85.48%

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ class SimpleCNN(nn.Module):
         conv2_size = 5
         conv3_size = 3
         self.conv1 = nn.Conv2d(3, 16, conv1_size, padding=conv1_size//2)
+        self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, conv2_size, padding=conv2_size//2)
         self.conv3 = nn.Conv2d(32, 64, conv3_size, padding=conv3_size//2)
         self.pool = nn.MaxPool2d(2, 2)
@@ -16,9 +17,19 @@ class SimpleCNN(nn.Module):
         self.fc2 = nn.Linear(512, 3)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        
         x = x.view(-1, 64 * 37 * 37)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
